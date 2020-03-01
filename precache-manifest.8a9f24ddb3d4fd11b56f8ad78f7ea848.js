0caa81e01f1baf2655fef4955bfea34d9f8f52fc
Patrick <torralbapatrick15@gmail.com>
2020-03-01 15:49:50 -0600

    Updates

---
+++ precache-manifest.8a9f24ddb3d4fd11b56f8ad78f7ea848.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25f0c99ede9e00706ba9b63b0b1aac3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ecabd4876e34289fc42",
+    "url": "/static/css/main.6895fcd3.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ecabd4876e34289fc42",
+    "url": "/static/js/main.9d8b599b.chunk.js"
+  },
+  {
+    "revision": "8963f78c0e1a10b5862f",
+    "url": "/static/js/runtime-main.8b07e775.js"
+  }
+]);
